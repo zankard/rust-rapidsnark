0e82ec978c603677f7d5badc7ea91e7c8b2917ab
Rex Fernando <rex1fernando@gmail.com>
2024-03-04 19:32:03 -0800

    work from during attempt to merge to aptos-core

--- build.rs
+++ build.rs
@@ -1,38 +1,24 @@
-use std::env;
-use std::path::PathBuf;
-use std::process::Command;
+// Copyright © Aptos Foundation
+
+use std::{env, path::PathBuf, process::Command};
 
 fn main() {
-    println!("{}", env::current_dir().expect("Couldn't get working dir.").to_str().expect("couldn't convert pathbuf to str."));
-    println!("Hi!");
-    println!("Hi!");
-    println!("Hi!");
-    println!("Hi!");
-    println!("Hi!");
-    println!("Hi!");
-    println!("Hi!");
-    println!("Hi!");
+    println!(
+        "{}",
+        env::current_dir()
+            .expect("Couldn't get working dir.")
+            .to_str()
+            .expect("couldn't convert pathbuf to str.")
+    );
 
-
-    println!("{:?}", Command::new("bash")
-        .arg("-c")
-        .arg("cd rapidsnark && ./build_lib.sh")
-        .output()
-        .expect("Failed to build c++ library"));
-
-
-
-
-    let libdir_path = PathBuf::from("rapidsnark/package/lib")
-    // Canonicalize the path as `rustc-link-search` requires an absolute
-    // path.
-    .canonicalize()
-    .expect("cannot canonicalize libdir path");
-    let include_path = PathBuf::from("wrapper.hpp")
-    // Canonicalize the path as `rustc-link-search` requires an absolute
-    // path.
-    .canonicalize()
-    .expect("cannot canonicalize include path");
+    println!(
+        "{:?}",
+        Command::new("bash")
+            .arg("-c")
+            .arg("cd rapidsnark && ./build_lib.sh")
+            .output()
+            .expect("Failed to build c++ library")
+    );
 
     // Tell cargo to tell rustc to link the system `clang`
     // shared library.
@@ -41,9 +27,6 @@ fn main() {
     println!("cargo:rerun-if-env-changed=OPENMP_LIBRARY_PATH");
 
     println!("cargo:rerun-if-env-changed=LIBCLANG_DYNAMIC_PATH");
-
-    println!("cargo:rustc-link-search=native=/usr/lib/llvm-14/lib");
-
 
     if let Ok(libclang_path) = env::var("LIBCLANG_PATH") {
         println!("cargo:rustc-link-search=native={}", libclang_path);
@@ -54,8 +37,13 @@ fn main() {
         println!("cargo:rustc-link-search=native={}", std_cpp_lib_path);
     }
 
+    println!("cargo:rustc-link-lib=dylib=gmp");
 
-
+    let libdir_path = PathBuf::from("rapidsnark/package/lib")
+        // Canonicalize the path as `rustc-link-search` requires an absolute
+        // path.
+        .canonicalize()
+        .expect("cannot canonicalize libdir path");
     // Tell cargo to look for shared libraries in the specified directory
     println!("cargo:rustc-link-search={}", libdir_path.to_str().unwrap());
 
@@ -63,18 +51,42 @@ fn main() {
     // shared library.
     println!("cargo:rustc-link-lib=static=rapidsnark-fr-fq");
 
-    // println!("cargo:rustc-link-lib=c++"); // This is needed on macos
+    os_specific_printlns();
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = build_bindings();
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
+
+#[cfg(not(target_os = "linux"))]
+fn os_specific_printlns() {
+    println!("cargo:rustc-link-lib=c++"); // This is needed on macos
+}
+
+#[cfg(target_os = "linux")]
+fn os_specific_printlns() {
     println!("cargo:rustc-link-lib=stdc++"); // This is needed on linux (will error on macos)
-                                
+    println!("cargo:rustc-link-search=native=/usr/lib/llvm-14/lib");
     println!("cargo:rustc-link-lib=dylib=omp");
     println!("cargo:rustc-link-lib=dylib=gomp");
-    println!("cargo:rustc-link-lib=dylib=gmp");
 
     println!("cargo:rustc-link-arg=-fopenmp");
+}
 
-
-
-
+#[cfg(not(target_os = "linux"))]
+fn build_bindings() -> bindgen::Bindings {
+    let include_path = PathBuf::from("wrapper.hpp")
+        // Canonicalize the path as `rustc-link-search` requires an absolute
+        // path.
+        .canonicalize()
+        .expect("cannot canonicalize include path");
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
@@ -85,12 +97,7 @@ fn main() {
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .clang_arg("-fopenmp")
-        .clang_arg("-L/usr/lib/llvm-14/lib")
         .clang_arg("-I./rapidsnark/package/include")
-        .clang_arg("-I/usr/lib/llvm-14/lib/clang/14.0.6/include")
-        .clang_arg(format!("-I/usr/include/c++/12/"))
-        .clang_arg(format!("-I/usr/include/x86_64-linux-gnu/c++/12/"))
         .clang_arg("-I./rapidsnark/depends/json/single_include")
         .clang_arg("-I./rapidsnark/depends/ffiasm/c")
         .clang_arg("-I./rapidsnark/build")
@@ -107,15 +114,57 @@ fn main() {
         .allowlist_type("ProverResponseType")
         .allowlist_type("ProverError")
         .allowlist_type("ProverResponseMetrics")
-
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+}
+
+#[cfg(target_os = "linux")]
+fn build_bindings() -> bindgen::Bindings {
+    let include_path = PathBuf::from("wrapper.hpp")
+        // Canonicalize the path as `rustc-link-search` requires an absolute
+        // path.
+        .canonicalize()
+        .expect("cannot canonicalize include path");
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header(include_path.to_str().unwrap())
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .clang_arg("-fopenmp")
+        .clang_arg("-L/usr/lib/llvm-14/lib")
+        .clang_arg("-I./rapidsnark/package/include")
+        .clang_arg("-I/usr/lib/llvm-14/lib/clang/14.0.6/include")
+        .clang_arg("-I/usr/include/c++/12/")
+        .clang_arg("-I/usr/include/x86_64-linux-gnu/c++/12/")
+        .clang_arg("-I./rapidsnark/depends/json/single_include")
+        .clang_arg("-I./rapidsnark/depends/ffiasm/c")
+        .clang_arg("-I./rapidsnark/build")
+        .clang_arg("-I./rapidsnark/src")
+        .clang_arg("-std=c++17")
+        .clang_arg("-stdlib=libc++")
+        .blocklist_file("alt_bn128.hpp")
+        .blocklist_file("groth16.hpp")
+        .blocklist_file("binfile_utils.hpp")
+        .blocklist_file("curve.hpp")
+        .blocklist_file("zkey_utils.hpp")
+        .allowlist_file("fullprover.hpp")
+        .allowlist_type("FullProver")
+        .allowlist_type("ProverResponseType")
+        .allowlist_type("ProverError")
+        .allowlist_type("ProverResponseMetrics")
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    bindings
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
+// Copyright © Aptos Foundation
+
+// Copyright © Aptos Foundation
+
+// Copyright © Aptos Foundation
 
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![allow(dead_code)]
 
 mod cpp {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
@@ -9,12 +15,10 @@ mod cpp {
     unsafe impl Send for FullProver {}
 }
 
-use std::ffi::{CString, CStr};
+use std::ffi::{CStr, CString};
 use thiserror::Error;
 
-
 pub type ProverResponse<'a> = Result<&'a str, ProverError>;
-
 
 #[derive(Debug, Error)]
 pub enum ProverInitError {
@@ -23,7 +27,7 @@ pub enum ProverInitError {
     #[error("Prover key is using an unsupported curve")]
     UnsupportedZKeyCurve,
     #[error("Unknown error")]
-    Unknown
+    Unknown,
 }
 
 #[derive(Debug, Error)]
@@ -37,47 +41,69 @@ pub enum ProverError {
     #[error("Witness generation outputted with an invalid curve")]
     WitnessGenerationInvalidCurve,
     #[error("Unknown error: {0}")]
-    Unknown(&'static str)
+    Unknown(&'static str),
 }
 
-
 pub struct FullProver {
-    _full_prover : cpp::FullProver
+    _full_prover: cpp::FullProver,
 }
 
 impl FullProver {
-    pub fn new(zkey_path : &str, witness_gen_binary_folder_path : &str) -> Result<FullProver, ProverInitError> {
-        let full_prover = unsafe { 
-            FullProver { 
-                _full_prover: cpp::FullProver::new(
-                                  CString::new(zkey_path).expect("CString::new failed").as_ptr(),
-                                  CString::new(witness_gen_binary_folder_path).expect("CString::new failed").as_ptr()
-                                  )
+    pub fn new(
+        zkey_path: &str,
+        witness_gen_binary_folder_path: &str,
+    ) -> Result<FullProver, ProverInitError> {
+        let zkey_path_cstr = CString::new(zkey_path).expect("CString::new failed");
+        let wgbfp_cstr = CString::new(witness_gen_binary_folder_path).expect("CString::new failed");
+        let full_prover = unsafe {
+            FullProver {
+                _full_prover: cpp::FullProver::new(zkey_path_cstr.as_ptr(), wgbfp_cstr.as_ptr()),
             }
         };
         match full_prover._full_prover.state {
             cpp::FullProverState_OK => Ok(full_prover),
             cpp::FullProverState_ZKEY_FILE_LOAD_ERROR => Err(ProverInitError::ZKeyFileLoadError),
-            cpp::FullProverState_UNSUPPORTED_ZKEY_CURVE => Err(ProverInitError::UnsupportedZKeyCurve),
-            _ => Err(ProverInitError::Unknown)
+            cpp::FullProverState_UNSUPPORTED_ZKEY_CURVE => {
+                Err(ProverInitError::UnsupportedZKeyCurve)
+            },
+            _ => Err(ProverInitError::Unknown),
         }
     }
 
-    pub fn prove(&mut self, input: &str) -> Result<(&str, cpp::ProverResponseMetrics), ProverError> {
-        let response = unsafe {
-            self._full_prover.prove(CString::new(input).expect("CString::new failed").as_ptr())
-        };
+    pub fn prove(
+        &mut self,
+        input: &str,
+    ) -> Result<(&str, cpp::ProverResponseMetrics), ProverError> {
+        let input_cstr = CString::new(input).expect("CString::new failed");
+        let response = unsafe { self._full_prover.prove(input_cstr.as_ptr()) };
         match response.type_ {
-            cpp::ProverResponseType_SUCCESS => unsafe { Ok((CStr::from_ptr(response.raw_json).to_str().expect("CStr::to_str failed"), response.metrics)) },
+            cpp::ProverResponseType_SUCCESS => unsafe {
+                Ok((
+                    CStr::from_ptr(response.raw_json)
+                        .to_str()
+                        .expect("CStr::to_str failed"),
+                    response.metrics,
+                ))
+            },
             cpp::ProverResponseType_ERROR => match response.error {
-                cpp::ProverError_NONE => Err(ProverError::Unknown("c++ rapidsnark prover returned \"error\" response type but error is \"none\"")),
+                cpp::ProverError_NONE => Err(ProverError::Unknown(
+                    "c++ rapidsnark prover returned \"error\" response type but error is \"none\"",
+                )),
                 cpp::ProverError_PROVER_NOT_READY => Err(ProverError::ProverNotReady),
                 cpp::ProverError_INVALID_INPUT => Err(ProverError::InvalidInput),
-                cpp::ProverError_WITNESS_GENERATION_BINARY_PROBLEM => Err(ProverError::WitnessGenerationBinaryProblem),
-                cpp::ProverError_WITNESS_GENERATION_INVALID_CURVE => Err(ProverError::WitnessGenerationInvalidCurve),
-                _ => Err(ProverError::Unknown("c++ rapidsnark prover returned an unknown error code"))
+                cpp::ProverError_WITNESS_GENERATION_BINARY_PROBLEM => {
+                    Err(ProverError::WitnessGenerationBinaryProblem)
+                },
+                cpp::ProverError_WITNESS_GENERATION_INVALID_CURVE => {
+                    Err(ProverError::WitnessGenerationInvalidCurve)
+                },
+                _ => Err(ProverError::Unknown(
+                    "c++ rapidsnark prover returned an unknown error code",
+                )),
             },
-            _ =>  Err(ProverError::Unknown("c++ rapidsnark prover returned an unknown error code"))
+            _ => Err(ProverError::Unknown(
+                "c++ rapidsnark prover returned an unknown error code",
+            )),
         }
     }
 }
@@ -87,30 +113,5 @@ impl Drop for FullProver {
         unsafe {
             self._full_prover.destruct();
         }
-    }
-}
-
-
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::fs;
-
-
-    #[test]
-    fn sanity_test() {
-        let arg1 = "/home/rex_fernando_aptoslabs_com/main_c.zkey";
-        let arg2 = "/home/rex_fernando_aptoslabs_com/main_c_cpp";
-        let input = fs::read_to_string("/home/rex_fernando_aptoslabs_com/input.json").expect("reading input.json failed");
-        unsafe { 
-            println!("starting test");
-            println!("calling constructor");
-            let mut full_prover = FullProver::new(arg1, arg2).expect("full_prover::new failed");
-            println!("calling prove method");
-            let response = full_prover.prove(input.as_str());
-            println!("response: {:?}", response);
-        }
-        
     }
 }
